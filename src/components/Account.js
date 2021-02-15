2b8f40b8682f60b82e0e44cc0af9032bf8ae2928
flaaaps <julianwening4@gmail.com>
2021-02-15 09:13:49 +0100

    :sparkles: Create account page & logout feature

---
+++ src/components/Account.js
@@ -0,0 +1,33 @@
+import React, { useContext } from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { UserContext } from '../App';
+
+function Account() {
+    const { user, setLoggedIn, setUser } = useContext(UserContext);
+
+    const logout = () => {
+        localStorage.removeItem('user');
+        console.log('Logged out');
+        setLoggedIn(false);
+        setUser({});
+    };
+    return (
+        <div className="w-50 mt-5 mx-auto">
+            <h1>Account</h1>
+            <p>
+                Name: <strong>{user.name}</strong>
+            </p>
+            <p>
+                Account created at: <strong>{new Date(user.createdAt).toLocaleDateString(window.navigator.language)}</strong>
+            </p>
+            <Button onClick={logout} className="my-2" style={{ width: '15rem' }}>
+                Logout
+            </Button>
+            <br />
+            <Link to="/">Back to home</Link>
+        </div>
+    );
+}
+
+export default Account;
